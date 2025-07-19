57e8fdfed8b5bd3dd89b9441e041c4e87c7a7175
mubarak <abubakrobasanjo@gmail.com>
2025-07-19 06:21:33 -0700

    modify layout page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,37 @@
-// app/layout.js (second site)
-// import ClientLayout from "./components/ClientLayout";
-import ClientLayout from "./ClientLayout";
 import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
+// app/components/DashboardLayout.tsx
+import ClientLayout from "./ClientLayout";
+import { Outfit } from "next/font/google";
+// import { useState } from "react";
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 export const metadata = {
   title: "Sinkronis",
   description: "Signup to get started",
 };
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"], // Customize as needed
+  variable: "--font-outfit",
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <ClientLayout>{children}</ClientLayout>
+    <html lang="en" className={outfit.variable}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ClientLayout>
+          <div>{children}</div>
+        </ClientLayout>
       </body>
     </html>
   );
