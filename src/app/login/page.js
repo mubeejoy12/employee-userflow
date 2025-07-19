01e3d714d9762c21d286ea1d63151dd5b175f5e4
mubarak <abubakrobasanjo@gmail.com>
2025-07-19 06:26:09 -0700

    and added more input box and its functionality

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,6 +1,8 @@
 "use client";
 
+import { useState } from "react";
 import { TextField, Button, Typography } from "@mui/material";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 export default function LoginPage() {
@@ -25,27 +27,45 @@ export default function LoginPage() {
       );
     }
   };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
       <div className="w-full max-w-sm bg-white p-6 rounded-lg shadow-md">
-        <Typography variant="h5" className="mb-8  font-semibold">
+        <Typography variant="h5" className="mb-8 font-semibold">
           Welcome Back
         </Typography>
 
-        <form className="space-y-4 mt-4 flex flex-col gap-4">
-          <TextField label="Email" type="email" fullWidth variant="outlined" />
+        <form
+          onSubmit={handleLogin}
+          className="space-y-4 mt-4 flex flex-col gap-4"
+        >
+          <TextField
+            label="Email"
+            type="email"
+            fullWidth
+            variant="outlined"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <TextField
             label="Password"
             type="password"
             fullWidth
             variant="outlined"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <Button variant="contained" fullWidth color="primary">
+          <Button type="submit" variant="contained" fullWidth color="primary">
             Login
           </Button>
         </form>
 
-        <Typography className="text-sm mt-4 ">
+        <Typography className="text-sm mt-4">
+          Try <strong>testuser@example.com</strong> /{" "}
+          <strong>password123</strong>
+        </Typography>
+
+        <Typography className="text-sm mt-4">
           Don’t have an account?{" "}
           <Link href="/signup" className="text-blue-600 hover:underline">
             Sign up
