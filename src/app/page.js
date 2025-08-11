afc2c25b2a1939dfd3b0b543c8fd79e244d60da7
mubarak <abubakrobasanjo@gmail.com>
2025-08-11 08:10:39 -0700

    remove import

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import CreatePassword from "./signup/page";
+
 import DashboardLayout from "./components/DashboardLayout";
 import { redirect } from 'next/navigation';
 // import { CreatePassword } from "./signup/page";
